64fcfe63b7eeb2d3d56e0edc6dc443e44f8c704e
Joao Franca <jvictorfranca@yahoo.com.br>
2026-09-21 18:55:43 -0300

    imports useref

--- src/app/chat/MessagesList.js
+++ src/app/chat/MessagesList.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { getMessages } from "./action"
 import { formatDistanceToNow } from "date-fns"
 
